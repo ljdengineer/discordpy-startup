5b9ef523addb2a932ff9800cf6477912368dd19b
ljdengineer <60516296+ljdengineer@users.noreply.github.com>
2020-02-01 04:02:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,21 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.event
+async def on_ready();
+    print('Rainbow Bot available!')
+
+    try;
+        home = bot.get_guild(671718259212681216)
+        role- home.get_role(672879406838054942)
+        while True;
+            color = [0xb35d5d, 0xb39a5d, 0x91b35d, 0x5db35f, 0x5db3a0, 0x5d90b3, 0x625db3, 0x965db3, 0xb35d9a]
+            r = random.choice(color)
+            colors = discord.Color(r)
+            await role.edit(colour=colors)
+            await asyncio.sleep(2)
+        except Exception as error:
+            print(coloring error)
 
 @bot.command()
 async def ping(ctx):
