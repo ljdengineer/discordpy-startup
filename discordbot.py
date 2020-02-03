7ab812101206deaf5202051e42ade5f24f3a2efa
ljdengineer <60516296+ljdengineer@users.noreply.github.com>
2020-02-03 21:44:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,28 @@
-import discord
+# LJD-bot 試験ビルド
+
 from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+
+bot = commands.Bot(command_prefix='/') #Botの接頭辞
+token = os.environ['DISCORD_BOT_TOKEN'] #herokuに設定されたtokenを適用させてるっぽい、おまじないの類
+
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx, error): #接頭辞付きの知らないコマンド来たら動く
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    await ctx.send(error_msg) #上で色々混ぜたのを出力してるらしい、難しい
+
 
 @bot.command()
 async def ping(ctx):
-    for(int i = 0; i <5; i++)
-        await ctx.send(f'No.{i} Latency: {round(bot.latency * 1000)}ms')
+    await ctx.send(f'No.{i} Latency: {round(bot.latency * 1000)}ms')
 
-bot.run(token)
+
+@bot.command()
+async def teemo(ctx):
+    await ctx.send('On duty!!')
+
+bot.run(token) #おまじない
