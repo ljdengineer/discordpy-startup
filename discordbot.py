14579285b2150ed0ee1273a48da5716a5a9b9517
ljdengineer <60516296+ljdengineer@users.noreply.github.com>
2020-02-03 20:07:31 +0900

    just test what I think

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
+"""
 @bot.event
 async def on_raw_reaction_add(payload): 
     channel = client.get_channel(payload.channel_id)  
@@ -27,12 +27,11 @@ async def on_raw_reaction_add(payload):
         role = guild.get_role(ID_ROLE_WELCOME)  
         await member.add_roles(role)  
         await channel.send('dekita')  
-
 """
+
 @bot.event
 async def on_raw_reaction_add(payload):
-    message_id = payload.message_id
-    if message_id == 673725675131240458:
+    if message.id == 673725675131240458:
         guild_id = payload.guild_id
         guild = discord.utils.find(lamba g : g.id == guild_id, client.guilds)
 
@@ -64,8 +63,7 @@ async def on_raw_reaction_add(payload):
 or
 @bot.event
 async def on_raw_reaction_remove(payload):
-    message_id = payload.message_id
-    if message_id == 673725675131240458:
+    if message.id == 673725675131240458:
         guild_id = payload.guild_id
         guild = discord.utils.find(lamba g : g.id == guild_id, client.guilds)
 
@@ -91,7 +89,7 @@ async def on_raw_reaction_remove(payload):
                 print("removed")
             else:
                 print("Role not found")
-"""
+
 
 @bot.command()
 async def ping(ctx):
