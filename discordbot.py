3531d9c78a226c90346f28f4791965c8962671d4
ljdengineer <60516296+ljdengineer@users.noreply.github.com>
2020-02-03 17:48:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
