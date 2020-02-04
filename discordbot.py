35461d1aec2bf7a5f8144761d8e7d73571cfd061
ljdengineer <60516296+ljdengineer@users.noreply.github.com>
2020-02-05 04:27:47 +0900

    ver1.20-test
    from
            await discord.Client().send_message(text_chat,msg)
    to
            await discord.bot().send_message(text_chat,msg)

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
 # LJD-bot 試験ビルド
-# Patch 1.18
+# Patch 1.20
 # 1.8辺りからon_readyを稼働させるのに必死
 # だったけどもしかしたらon_readyなのが悪いのかと考え始め
 # 他のon_xxx系を試してみる
@@ -12,7 +12,7 @@ import traceback
 bot = commands.Bot(command_prefix='/') #Botの接頭辞
 token = os.environ['DISCORD_BOT_TOKEN'] #herokuに設定されたtokenを適用させてるっぽい、おまじないの類
 
-### ver1.19 test
+### ver1.20 test
 channel = discord.utils.get(guild.text_channels, name='test01')
 
 # 初期搭載機能群ここから
@@ -36,11 +36,11 @@ async def on_ready(channel):
     await channel.send('Yay!')
 """
 
-### ver1.19 test
+### ver1.20 test
 @bot.event
 async def on_member_update(before, after):
     if before.status != after.status:
         msg = after.display_name + " さんが " + str(after.status) + " になりました"
-        await discord.Client().send_message(text_chat,msg)
+        await discord.bot().send_message(text_chat,msg)
     
 bot.run(token) #おまじない
